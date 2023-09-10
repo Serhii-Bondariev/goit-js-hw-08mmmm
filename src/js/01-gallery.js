d78d56ab9732e7d2cdb8c3775f04ebed87fdc363
Serhii-Bondariev <xx.code4u@gmail.com>
2023-09-11 00:09:56 +0300

    gallery+

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+function murkup(gallery) {
+  return gallery
+    .map((item) => {
+      console.log(item);
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+ </li>`;
+    })
+    .join("");
+}
 
-console.log(galleryItems);
-
-const galleryRef = document.querySelector('.gallery');
+gallery.insertAdjacentHTML("beforeend", murkup(galleryItems));
+console.log(murkup(galleryItems));
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
 
